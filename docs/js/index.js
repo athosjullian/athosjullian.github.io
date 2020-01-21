1e5eb0fafa0a0bc925003f7fcc7377578b2c6933
athosjullian <athosjullian@gmail.com>
2020-01-21 16:49:31 -0300

    Adicionando o projeto "EX - conversor de moedas"

--- docs/js/index.js
+++ docs/js/index.js
@@ -3,10 +3,12 @@ let body = document.querySelector('body');
 let keep = document.getElementById("keep");
 let seta = document.getElementById("seta");
 let cfo = document.getElementById("cfo");
+let ex = document.getElementById("ex");
 // botão que abre o modal
 let btnKeep = document.querySelectorAll(".projeto1");
 let btnSeta = document.querySelector(".projeto2");
 let btnCfo = document.querySelector(".projeto3");
+let btnEx = document.querySelector(".projeto4");
 
 
 // Abre o modal quando o usuário clica no botão
@@ -27,12 +29,18 @@ btnCfo.onclick = function() {
   body.style.overflow = "hidden";
 }
 
+btnEx.onclick = function() {
+  ex.classList.add('show');
+  body.style.overflow = "hidden";
+}
+
 // Quando o usuário clica em qualquer lugar fora do modal, fecha.
 window.onclick = function(event) {
-    if (event.target == keep || event.target == seta || event.target == cfo) {
+    if (event.target == keep || event.target == seta || event.target == cfo || event.target == ex) {
         keep.classList.remove('show');
         seta.classList.remove('show');
         cfo.classList.remove('show');
+        ex.classList.remove('show');
         
         body.style.overflowY = "scroll";
     }
@@ -47,6 +55,7 @@ span.forEach((s) => {
         keep.classList.remove('show');
         seta.classList.remove('show');
         cfo.classList.remove('show');
+        ex.classList.remove('show');
 
         body.style.overflowY = "scroll";
     });
